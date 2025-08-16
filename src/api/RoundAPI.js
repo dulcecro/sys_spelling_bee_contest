5307bc9d42f9c3f285e3cb21f5e40ccb042c2dd1
Juan Guevara M. <juanexgue@gmail.com>
2025-08-15 23:57:14 -0500

    No save Score

--- src/api/RoundAPI.js
+++ src/api/RoundAPI.js
@@ -1,5 +1,5 @@
 import api from './axiosSetup'
 
 export const getRoundsByGroupId = (idGrade) => api.get('/round/1/' + idGrade)
-export const saveScore = () => api.get('/round/1/16')
+// export const saveScore = () => api.get('/round/1/16')
 export const getGroups = () => api.get('/grade')
